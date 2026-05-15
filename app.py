1c6c4d01459637c53ae90532eb18074e97d75064
Joshua Darrah <darrahjoshua551@gmail.com>
2026-05-15 21:39:50 +0000

    Updated index.html and app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "uploads"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Home Page
 @app.route("/")
 def home():
-    return "AI Resume Screening System is running"
+    return render_template("index.html")
 
+
+# Upload Route
+@app.route('/uploads', method=['POST'])
+def upload_resume():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
