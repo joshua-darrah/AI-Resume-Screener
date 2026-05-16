752c7e0b8e3188922b23e46e7aff2da67ef7f99c
Joshua Darrah <darrahjoshua551@gmail.com>
2026-05-16 15:18:31 +0000

    corrected file.Save to file.save

--- app.py
+++ app.py
@@ -13,9 +13,24 @@ def home():
 
 
 # Upload Route
-@app.route('/uploads', method=['POST'])
+@app.route('/uploads', methods=['POST'])
 def upload_resume():
-    pass
+
+    # Check if file exist
+    if "resume" not in request.files:
+        return "No files uploaded"
+
+    file = request.files['resume']
+
+    # Check if filename is empty
+    if file.filename == '':
+        return "No selected file"
+
+    # Save file
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+
+    return f"Resume uploaded successfully: {file.filename}"
 
 if __name__ == "__main__":
     app.run(debug=True)
