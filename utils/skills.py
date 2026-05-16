32ca581d421bb93d1c0bcf088d60978c217e674c
Joshua Darrah <darrahjoshua551@gmail.com>
2026-05-16 16:39:53 +0000

    Created skills database

---
+++ utils/skills.py
@@ -0,0 +1,44 @@
+SKILLS = [
+
+    # Programming
+    "python",
+    "java",
+    "c",
+    "c++",
+    "javascript",
+    "typescript",
+
+    # Web Development
+    "html",
+    "css",
+    "flask",
+    "django",
+    "react",
+    "node.js",
+
+    # Databases
+    "sql",
+    "mysql",
+    "postgresql",
+    "mongodb",
+
+    # AI/ML
+    "machine learning",
+    "deep learning",
+    "artificial intelligence",
+    "tensorflow",
+    "pytorch",
+    "scikit-learn",
+    "nlp",
+
+    # Tools
+    "git",
+    "github",
+    "docker",
+    "linux",
+
+    # Cloud
+    "aws",
+    "azure",
+    "gcp"
+]
